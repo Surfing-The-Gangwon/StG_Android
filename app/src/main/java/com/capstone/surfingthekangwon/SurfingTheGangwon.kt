11a9d7164d1dbea85f3ca233e1d0a9238b4da3fe
arieum <lar713@naver.com>
2025-07-22 10:22:15 +0900

    Initial commit

---
+++ app/src/main/java/com/capstone/surfingthekangwon/SurfingTheGangwon.kt
@@ -0,0 +1,12 @@
+package com.capstone.surfingthekangwon
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SurfingTheGangwon: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ presentation/src/main/java/com/capstone/presentation/EmptyViewModel.kt
@@ -0,0 +1,10 @@
+package com.capstone.presentation
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class EmptyViewModel : ViewModel() {
+    // This ViewModel is intentionally left empty.
+    // It can be used as a placeholder or for testing purposes.
+}
